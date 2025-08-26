8f97418e74814422407b8d645c29c73ca4c5ef1e
xs121913958 <xs121913958@163.com>
2025-08-26 15:48:04 +0800

    feat: codemagic.yaml配置文件中新增脚本来更新应用图标、应用名称、应用启动图以及修改项目的webUrl.dart文件内容

--- lib/webUrl.dart
+++ lib/webUrl.dart
@@ -1,6 +1,2 @@
-
-/// https://reimagined-memory-jjgwj4xwqgxrfq4p4-8080.app.github.dev
-/// http://192.168.18.182
-/// https://www.kkgametop.xyz
-/// https://www.ccgametest.live
-const webUrl = 'https://new.ccgametest.live';
+const String webUrl = 'https://new.ccgametest.live';
+const String jpushAppKey = '8b68b9303424eadca9e99ae9';
