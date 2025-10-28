2d88405a7dc2c9b64318ef2b2c0ffef9a91325e8
xs121913958 <xs121913958@163.com>
2025-10-28 22:19:17 +0800

    深度链接

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_links/app_links.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -29,20 +30,69 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AppLinks _appLinks = AppLinks();
+
+  @override
+  void initState() {
+    super.initState();
+    _initAppLinks(); // 初始化链接监听
+  }
+
+  // 初始化链接监听
+  void _initAppLinks() async {
+    // 1. 监听应用冷启动时的初始链接（如从网页唤起未运行的应用）
+    final initialUri = await _appLinks.getInitialLink();
+    if (initialUri != null) {
+      _handleUri(initialUri);
+    }
+
+    // 2. 监听应用已启动（热启动）时的链接
+    _appLinks.uriLinkStream.listen((uri) {
+      _handleUri(uri);
+    });
+  }
+
+  // 处理链接逻辑（解析参数、跳转页面等）
+  void _handleUri(Uri uri) {
+    if (kDebugMode) {
+      print('唤起链接: ${uri.toString()}');
+    }
+
+    // 解析链接参数（示例：ccgameapp://open.native.app/page?page=detail&id=123）
+    // final path = uri.path; // 取值：/page
+    // final page = uri.queryParameters['page']; // 取值：detail
+    // final id = uri.queryParameters['id']; // 取值：123
+
+    // 根据参数执行跳转
+    // if (page == 'detail' && id != null) {
+    //   Navigator.push(
+    //     context,
+    //     MaterialPageRoute(
+    //       builder: (context) => DetailPage(id: id),
+    //     ),
+    //   );
+    // }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'KK Game',
+      title: 'CC Game',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       navigatorObservers: [FlutterSmartDialog.observer],
       builder: FlutterSmartDialog.init(),
-      home: const HomePage(title: 'KK Game'),
+      home: const HomePage(title: 'CC Game'),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import file_selector_macos
 import flutter_inappwebview_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
 }
